075711a712061d1d93c96546672693d798cf9b79
YUJIWAN <61855068+YUJIWAN@users.noreply.github.com>
2020-03-19 10:42:11 +0900

    첫번째 프래그 두번째 프래그 누르면 보여준다

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/MainActivity.java
@@ -3,7 +3,9 @@ package kr.co.tjoeun.a20200319_01_fragment01;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.co.tjoeun.a20200319_01_fragment01.databinding.ActivityMainBinding;
 
@@ -22,6 +24,31 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.firstFragCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                첫 프래그먼트 호출 버튼이 눌리면?
+//                firstFrag보이게
+                binding.firstFragLayout.setVisibility(View.VISIBLE);
+            //                        secondFrog숨기게
+                binding.secondFragLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        binding.secondFragCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                첫 프래그 : 숨겨줌
+//                두번째 프래그 나타남
+                binding.firstFragLayout.setVisibility(View.GONE);
+                binding.secondFragLayout.setVisibility(View.VISIBLE);
+
+            }
+        });
 
     }
 
